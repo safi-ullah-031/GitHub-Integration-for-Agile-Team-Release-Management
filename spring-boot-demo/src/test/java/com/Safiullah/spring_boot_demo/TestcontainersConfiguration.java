58307a52eedbb2d8f3cd8679915f8626e9f48f06
Safi Ullah <safiullah445@outlook.com>
2025-04-26 16:53:47 +0500

    testing using the external file

--- spring-boot-demo/src/test/java/com/Safiullah/spring_boot_demo/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.Safiullah.spring_boot_demo;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
